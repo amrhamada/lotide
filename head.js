111ae31d0701134f019f3199f220971db418ce92
Amro Abdelaziz <amr.hamada93@gmail.com>
2020-03-30 20:34:55 +0000

    return tail of an array

--- head.js
+++ head.js
@@ -10,7 +10,7 @@ const assertEqual = (actual, expected)=>{
 
 const head = (list) =>{
   return list[0];
-}
+};
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
