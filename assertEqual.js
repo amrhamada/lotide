1bba54a05f03a4291ed5253291a562d7053cd570
Amro Abdelaziz <amr.hamada93@gmail.com>
2020-03-30 20:14:55 +0000

    changed to string literals

--- assertEqual.js
+++ assertEqual.js
@@ -2,9 +2,9 @@ const pass = String.fromCodePoint(0x1F7E2);
 const fail = String.fromCodePoint(0x1F534);
 const assertEqual = (actual, expected)=>{
   if (actual === expected) {
-    console.log(pass + pass + pass + ' ' + "Assertion Passed: " + actual + " === " + expected);
+    console.log(`${pass}${pass}${pass} Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(fail + fail + fail + ' ' + "Assertion Failed: " + actual + " !== " + expected);
+    console.log(`${fail}${fail}${fail} Assertion Failed: ${actual} === ${expected}`);
   }
 };
 
