350720a39d81b7f2387f73a58695b7a5d4bc1d3a
Amro Abdelaziz <amr.hamada93@gmail.com>
2020-04-01 18:01:37 +0000

    countOnly function - return count for items

---
+++ countOnly.js
@@ -0,0 +1,41 @@
+const pass = String.fromCodePoint(0x1F7E2);
+const fail = String.fromCodePoint(0x1F534);
+const assertEqual = (actual, expected)=>{
+  if (actual === expected) {
+    console.log(`${pass}${pass}${pass} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${fail}${fail}${fail} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countOnly = (allItems, itemsToCount) => {
+  const results = {};
+
+  for (const item of allItems) {
+    if (itemsToCount[item]) { 
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
