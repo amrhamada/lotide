3883888ad2626a426c67a25d8ed2bb87f1d331b5
Amro Abdelaziz <amr.hamada93@gmail.com>
2020-04-01 18:53:39 +0000

    find key by value

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+const pass = String.fromCodePoint(0x1F7E2);
+const fail = String.fromCodePoint(0x1F534);
+const assertEqual = (actual, expected)=>{
+  if (actual === expected) {
+    console.log(`${pass}${pass}${pass} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${fail}${fail}${fail} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = (obj, val) => {
+  for (const key in obj){
+    if (obj[key] === val ){
+      return key;
+    }
+  }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+
