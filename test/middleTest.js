7e84f1f0a49b77b7629404e6faf2099b230160a8
Amro Abdelaziz <amr.hamada93@gmail.com>
2020-04-06 19:16:10 +0000

    chagning testing to mocha

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,36 @@
-const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
+const should = require('chai').should();
 
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2,3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]); // => [3, 4]
+describe("#middle", () => {
+  it('returns [] for [1]', () => {
+    output = middle([1]);
+    output.should.deep.equal([]);
+  });
+  it('returns [] for [1, 2]', () => {
+    output = middle([1, 2]);
+    output.should.deep.equal([]);
+  });
+  it('for [1, 2, 3] return [2]', () => {
+    output = middle([1,2,3]);
+    output.should.deep.equal([2]);
+  });
+  it('for [1, 2, 3, 4, 5] returns [3]', () => {
+    output = middle([1, 2, 3, 4, 5]);
+    output.should.deep.equal([3]);
+  });
+  it('for [1, 2, 3, 4] returns [2,3]', () => {
+    output = middle([1, 2, 3, 4]);
+    output.should.deep.equal([2, 3]);
+  });
+  it('for [1, 2, 3, 4, 5, 6] returns [3,4]', () => {
+    output = middle([1, 2, 3, 4, 5, 6]);
+    output.should.deep.equal([3,4]);
+  });
+});
+
+// middle([1]), []); // => []
+// middle([1, 2]), []); // => []
+// middle([1, 2, 3]), [2]); // => [2]
+// middle([1, 2, 3, 4, 5]), [3]); // => [3]
+// middle([1, 2, 3, 4]), [2,3]); // => [2, 3]
+// middle([1, 2, 3, 4, 5, 6]), [3,4]); // => [3, 4]
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,12 +1,14 @@
-const assertEqual = require('../assertEqual');
 const tail = require('../tail');
+const assert = require('chai').assert;
+const should = require('chai').should();
 
+describe("#tail", () => {
+  it('returns ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]', () => {
+    output = tail(["Hello", "Lighthouse", "Labs"]);
+    output.should.equal(output,["Lighthouse", "Labs"]);
+    output.should.have.lengthOf(2);
+    output[0].should.equal('Lighthouse');
+    output[1].should.equal('Labs');
+  });
+});
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
